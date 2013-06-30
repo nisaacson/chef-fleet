957c9f7d822d61225760816478f99823b642a16e
Noah Isaacson <noah@nisaacson.com>
2013-06-29 22:49:56 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures fleet'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends "chef-node-web-user"
 depends "lowport-node"
